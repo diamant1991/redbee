31c6903865adfc360cdeb2b6ed5a70f82fe88c19
diamant1991 <diamant1988@mail.ru>
2017-08-18 10:35:41 +0300

    add main page

---
+++ app/js/main.js
@@ -0,0 +1,56 @@
+$('.navbar-toggle').click(function(e) {
+  e.preventDefault()
+  var menu = $(this).next()
+  if(menu.is(":hidden")){
+    $(this).addClass('active')
+    menu.slideDown(250)
+  }
+  else{
+    $(this).removeClass('active')
+    menu.slideUp(250)
+  }
+});
+
+$('#main-slider').owlCarousel({
+  loop:true,
+  margin: 0,
+  nav:false,
+  responsive:{
+    0:{
+        items:1
+    }
+  }
+})
+$('#services-carousel').owlCarousel({
+  loop:true,
+  margin: 20,
+  nav:true,
+  navText: ["<div class='carousel-btn carousel-btn--prev'><i class='fa fa-long-arrow-left'></i>предыдущие</div>", 
+            "<div class='carousel-btn carousel-btn--next'>следующие<i class='fa fa-long-arrow-right'></i></div>"],
+  responsive:{
+    0:{
+        items:1
+    },
+    767:{
+        items:2
+    },
+    991:{
+        items:3
+    },
+    1199:{
+        items:4
+    }
+  }
+})
+$('.review__carousel').owlCarousel({
+  loop:true,
+  margin: 0,
+  nav:true,
+  navText: ["<div class='carousel-btn carousel-btn--prev'><i class='fa fa-long-arrow-left'></i>предыдущий</div>", 
+            "<div class='carousel-btn carousel-btn--next'>следующий<i class='fa fa-long-arrow-right'></i></div>"],
+  responsive:{
+    0:{
+        items:1
+    }
+  }
+})
